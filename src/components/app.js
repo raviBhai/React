166025d61c051a17c3f2ca8bde873a0db06af582
Ravi Agrawal <ravi.agrawal@fidelis-inc.com>
2015-09-22 20:43:54 +0530

    Adding Router

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+/*"use strict";*/
+
+var React = require('react');
+var Header = require('./common/header');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+	render : function(){
+			return (
+					<div>
+						<Header/>
+						<div className="container-fluid">
+							<RouteHandler/>
+						</div>
+					</div>
+				);
+	}
+});
+
+module.exports = App;
--- src/main.js
+++ src/main.js
@@ -1,39 +1,9 @@
-$ = jQuery = require('jquery');
+"use strict";
+
 var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win){
-
-"use strict"
-
-var App = React.createClass({
-	render : function(){
-			var Child;
-			switch(this.props.route){
-				case 'about' : 
-					Child = About;
-					break;
-
-				default:
-					Child = Home;
-			}
-			return (
-					<div>
-						<Header/>
-						<Child/>
-					</div>
-				);
-	}
+Router.run(routes, function(Handler){
+	React.render(<Handler/>, document.getElementById('app'));
 });
-
-function render(){
-	var route = win.location.hash.substr(1);
-	React.render(<App route={route}/>, document.getElementById('app'));	
-}
-
-win.addEventListener('hashchange', render);
-render();
-
-})(window);
